aefe0077f687fcdc2a4e8b6ca4f7fc143fb61f7e
Trav Johnston <trav@touchpointglobal.com>
2017-07-20 17:10:34 +1000

    stacked charts

--- resources/views/chartjs/donut.blade.php
+++ resources/views/chartjs/donut.blade.php
@@ -19,17 +19,28 @@
                         {{ $dta }},
                     @endforeach
                 ],
-                backgroundColor: [
+	            borderColor: [
                     @if($model->colors)
-                        @foreach($model->colors as $color)
-                            "{{ $color }}",
-                        @endforeach
-                    @else
-                        @foreach($model->values as $dta)
-                            "{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}",
-                        @endforeach
+                            @foreach($model->colors as $color)
+			            window.chartColors.{{$color}},
+                    @endforeach
                     @endif
-                ],
+	            ],
+
+	            backgroundColor: [
+		            @if($model->colors)
+                            @foreach($model->colors as $color)
+                                color(window.chartColors.{{ $color }}).alpha(0.5).rgbString(),
+                    @endforeach
+                            @else
+
+                            @foreach($model->values as $dta)
+			            "{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}",
+                    @endforeach
+                    @endif
+	            ],
+
+	            borderWidth: 1,
             }]
         },
         options: {
--- resources/views/chartjs/pie.blade.php
+++ resources/views/chartjs/pie.blade.php
@@ -3,45 +3,56 @@
 @endif
 
 <script type="text/javascript">
-    var ctx = document.getElementById("{{ $model->id }}")
+	var ctx = document.getElementById("{{ $model->id }}")
 
-    var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: [
+	var myChart = new Chart(ctx, {
+		type: 'pie',
+		data: {
+			labels: [
                 @foreach($model->labels as $label)
-                    "{!! $label !!}",
+					"{!! $label !!}",
                 @endforeach
-            ],
-            datasets: [{
-                data: [
+			],
+			datasets: [{
+				data: [
                     @foreach($model->values as $dta)
-                        {{ $dta }},
+                    {{ $dta }},
                     @endforeach
-                ],
-                backgroundColor: [
+				],
+				borderColor: [
                     @if($model->colors)
-                        @foreach($model->colors as $color)
-                            "{{ $color }}",
-                        @endforeach
-                    @else
-                        @foreach($model->values as $dta)
-                            "{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}",
-                        @endforeach
+                            @foreach($model->colors as $color)
+						window.chartColors.{{$color}},
+                    @endforeach
                     @endif
-                ],
-            }]
-        },
-        options: {
-            responsive: {{ $model->responsive || !$model->width ? 'true' : 'false' }},
-            maintainAspectRatio: false,
+				],
+
+				backgroundColor: [
+					@if($model->colors)
+                            @foreach($model->colors as $color)
+                                color(window.chartColors.{{ $color }}).alpha(0.5).rgbString(),
+                    @endforeach
+                            @else
+
+                            @foreach($model->values as $dta)
+						"{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}",
+                    @endforeach
+                    @endif
+				],
+
+				borderWidth: 1,
+			}]
+		},
+		options: {
+			responsive: {{ $model->responsive || !$model->width ? 'true' : 'false' }},
+			maintainAspectRatio: false,
             @if($model->title)
-                title: {
-                    display: true,
-                    text: "{!! $model->title !!}",
-                    fontSize: 20,
-                }
+			title: {
+				display: true,
+				text: "{!! $model->title !!}",
+				fontSize: 20,
+			}
             @endif
-        }
-    });
+		}
+	});
 </script>
