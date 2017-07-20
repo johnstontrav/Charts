374b3fc5c7304fdc8a584021adf442a30cbf3e1f
Trav Johnston <trav@touchpointglobal.com>
2017-07-20 16:53:40 +1000

    stacked charts

--- resources/views/chartjs/bar.blade.php
+++ resources/views/chartjs/bar.blade.php
@@ -4,64 +4,67 @@
 
 <script type="text/javascript">
 
-    var ctx = document.getElementById("{{ $model->id }}")
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: [
+	var ctx = document.getElementById("{{ $model->id }}")
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: [
                 @foreach($model->labels as $label)
-                    "{!! $label !!}",
+					"{!! $label !!}",
                 @endforeach
-            ],
-            datasets: [
-                {
-                    label: "{!! $model->element_label !!}",
-	                borderColor: [
-		                @if($model->colors)
-                                @foreach($model->colors as $color)
-			                window.chartColors.{{$color}},
-                        @endforeach
-                    ]
-                    ,
-	                borderWidth: 1,
-	                backgroundColor: [
+			],
+			datasets: [
+				{
+					label: "{!! $model->element_label !!}",
+
+					borderColor: [
                         @if($model->colors)
                                 @foreach($model->colors as $color)
-			                color(window.chartColors.{{ $color }}).alpha(0.5).rgbString(),
-                        @endforeach
-                                @else
-                                @foreach($model->values as $dta)
-			                "{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}",
+							window.chartColors.{{$color}},
                         @endforeach
                         @endif
-	                ],
+					],
 
-                    data: [
-                        @foreach($model->values as $dta)
-                            {{ $dta }},
+					backgroundColor: [
+						@if($model->colors)
+                                @foreach($model->colors as $color)
+                                    color(window.chartColors.{{ $color }}).alpha(0.5).rgbString(),
                         @endforeach
-                    ],
-                }
-            ]
-        },
-        options: {
-            responsive: {{ $model->responsive || !$model->width ? 'true' : 'false' }},
-            maintainAspectRatio: false,
+                                @else
+
+                                @foreach($model->values as $dta)
+							"{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}",
+                        @endforeach
+                        @endif
+					],
+
+					borderWidth: 1,
+					data: [
+                        @foreach($model->values as $dta)
+                        {{ $dta }},
+                        @endforeach
+					],
+				}
+			]
+		},
+		options: {
+			responsive: {{ $model->responsive || !$model->width ? 'true' : 'false' }},
+			maintainAspectRatio: false,
             @if($model->title)
-            title: {
-                display: true,
-                text: "{!! $model->title !!}",
-                fontSize: 20,
-            },
+			title: {
+				display: true,
+				text: "{!! $model->title !!}",
+				fontSize: 20,
+			},
             @endif
-            scales: {
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        beginAtZero: true,
-                    }
-                }]
-            }
-        }
-    });
+			scales: {
+				yAxes: [{
+					display: true,
+					ticks: {
+						beginAtZero: true,
+					}
+				}]
+			}
+		}
+	});
 </script>
